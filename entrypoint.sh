33614498e65422a90ad664b41651279f8f228f92
Zachary Rice <zricer@protonmail.com>
2020-02-25 09:03:28 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,5 +25,6 @@ echo "nejjjjjjjjw pr"
 # aws_access_key_id='AKIAIO5FODNN7EXAMPLE'
 # aws_access_key_id='AKIAIO5FODNN7EXAMPLE'
 # aws_access_key_id='AKIAIO5FODNN7EXAMPLE'
+# aws_access_key_id='AKIAIO5FODNN7EXAkPLE'
 
 
