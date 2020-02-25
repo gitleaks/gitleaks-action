0173d0fa99213c32db797459f430eeab1c376b46
Zachary Rice <zricer@protonmail.com>
2020-02-24 20:14:21 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,8 @@ git --git-dir="$GITHUB_WORKSPACE/.git" branch
 git --git-dir="$GITHUB_WORKSPACE/.git" branch -a
 
 echo "anything?"
+echo "nejjjjjjjjw pr
+echo "nejjjjjjjjw pr"
 echo "nejjjjjjjjw pr"
 
 # aws_access_key_id='AKIAIO5FODNN7EXAMPLE'
