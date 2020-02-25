161b7abbb97c26aec344e40704d1d37c2ebdc6a4
Zachary Rice <zricer@protonmail.com>
2020-02-25 10:26:50 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,8 @@ if [ "$GITHUB_EVENT_NAME" = "pull_request" ]
 then 
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
   cat commit_list.txt
-  echo "commit list"
-  echo gitleaks --repo-path=$GITHUB_WORKSPACE --verbose --redact --branch=$GITHUB_HEAD_REF --commit-from="$(tail -n 1 commit_list.txt)" --commit-to="$(head -n 1 commit_list.txt)"
-  gitleaks --repo-path=$GITHUB_WORKSPACE --verbose --redact --branch=$GITHUB_HEAD_REF --commit-from="$(tail -n 1 commit_list.txt)" --commit-to="$(head -n 1 commit_list.txt)"  
+  echo gitleaks --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit-from="$(head -n 1 commit_list.txt)" --commit-to="$(tail -n 1 commit_list.txt)"
+  gitleaks --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit-from="$(head -n 1 commit_list.txt)" --commit-to="$(tail -n 1 commit_list.txt)"  
 fi 
 
 
