876563be23912e0a36a75810837aa4784793f0dd
gmti-bwhyle <mwhyle@gannett.com>
2020-08-28 11:42:54 -0400

    add variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,20 @@ fi
 echo running gitleaks "$(gitleaks --version) with the following command👇"
 
 DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
+GITLEAKS_RESULTS="test"
 
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
   echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG
   gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
-then 
+then
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
-  echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit-from="$(head -n 1 commit_list.txt)" --commit-to="$(tail -n 1 commit_list.txt)" $CONFIG 
+  echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit-from="$(head -n 1 commit_list.txt)" --commit-to="$(tail -n 1 commit_list.txt)" $CONFIG
   gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit-from="$(head -n 1 commit_list.txt)" --commit-to="$(tail -n 1 commit_list.txt)" $CONFIG
-fi 
-
+fi
+GITLEAKS_RESULTS=$?
+echo $GITLEAKS_RESULTS
 if [ $? -eq 1 ]
 then
   echo -e "\e[31m🛑 STOP! Gitleaks encountered leaks"
@@ -33,4 +35,3 @@ else
   echo "------------------------------------"
   echo -e $DONATE_MSG
 fi
-
