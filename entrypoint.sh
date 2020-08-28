856cd279d81386385fe7665a84d5505852d99290
gmti-bwhyle <mwhyle@gannett.com>
2020-08-28 16:05:17 -0400

    106

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ fi
 echo running gitleaks "$(gitleaks --version) with the following command👇"
 
 DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
-GITLEAKS_RESULTS="test"
+time=$(date)
+echo "::set-output name=time::$time"
 
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
