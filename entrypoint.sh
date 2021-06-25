676b460b9594ba42e8480d0879a277c8dcd72810
Zach Stone <zach@giantswarm.io>
2021-06-25 12:21:00 +0200

    Squashed commit of the following:
    commit 692d434a4e2513443db7d185f09c35158ca9db64
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 12:09:21 2021 +0200
    
        A fourth
    
    commit e99527c1c69c56d178713102a68131d58850c98e
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 12:09:11 2021 +0200
    
        A third
    
    commit 817a945956fb1225b366a5362d7b3129d53db403
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 12:08:55 2021 +0200
    
        Another commit
    
    commit 06ad4e304d1ada9bf618e8e82a35821c2a588415
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 12:08:40 2021 +0200
    
        A commit
    
    commit bfe911edce6aec9291add590ed263e3b08c83ada
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 12:05:04 2021 +0200
    
        Cleanup
    
    commit 12a257d89f59dc9844c4ab9002c51ec3b31fd44e
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 11:57:08 2021 +0200
    
        Use case
    
    commit ad1ee371fbf08564740fc40d4cadf8858fec1bf6
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 11:46:31 2021 +0200
    
        No remotes/origin/ when using before-commit
    
    commit 6126a848c0f1f936a33445d8d985da2537358f7c
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 11:41:47 2021 +0200
    
        Refactor
    
    commit 9a56f9ed0dec08ff5b1a12b19cd29127fd6d25a9
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 11:32:55 2021 +0200
    
        Restore arg order
    
    commit 5ebac253096f46267ca4ed039c37f476b4873078
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 11:03:24 2021 +0200
    
        Try direct parameter
    
    commit 13066f5e6d253fe809c21b2e8cbac880d0a151fe
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Fri Jun 25 10:30:23 2021 +0200
    
        Try changing argument order
    
    commit f0570bd8305636186d9f860949bdd19ef35adece
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Thu Jun 24 14:17:41 2021 +0200
    
        Debug stuff
    
    commit 7c3a6e4a8bb81438e388e23deee6600cfd5efd84
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Thu Jun 24 14:12:06 2021 +0200
    
        Use branch to test locally
    
    commit 69525d2c2206c8f22cd92a92e399c27c547281d7
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Thu Jun 24 14:10:32 2021 +0200
    
        Add 'commit-before' argument
    
    commit 43dc833b55aab06660da788cfdf70190ff6574c7
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Wed Jun 23 14:51:37 2021 +0200
    
        Commit to add a sample event
    
    commit 8c1763130bbccd2d827ec30f53f27ab5efd1463c
    Author: Zach Stone <zach@giantswarm.io>
    Date:   Wed Jun 23 14:49:13 2021 +0200
    
        A commit with some debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 INPUT_CONFIG_PATH="$1"
+INPUT_COMMIT_BEFORE="$2"
 CONFIG=""
 
-# check if a custom config have been provided
+# Check if a custom config has been provided.
 if [ -f "$GITHUB_WORKSPACE/$INPUT_CONFIG_PATH" ]; then
   CONFIG=" --config-path=$GITHUB_WORKSPACE/$INPUT_CONFIG_PATH"
 fi
 
-echo running gitleaks "$(gitleaks --version) with the following command👇"
-
 DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
 
-if [ "$GITHUB_EVENT_NAME" = "push" ]
-then
-  echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact $CONFIG
-  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact $CONFIG)
-elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
-then 
-  git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF... > commit_list.txt
-  echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
-  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG)
-fi
+# Use the base branch for pull requests and the "before" commit for pushes as the scan boundary.
+case "$GITHUB_EVENT_NAME" in
+ "push")         TARGET_REF="$INPUT_COMMIT_BEFORE" ;;
+ "pull_request") TARGET_REF="remotes/origin/$GITHUB_BASE_REF" ;;
+ *)              echo "Unsupported event type: $GITHUB_EVENT_NAME"; exit 1 ;;
+esac
+
+echo "Scanning commits back to $TARGET_REF"
+
+# Create list of commits since the target ref.
+git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" $TARGET_REF... > commit_list.txt
+
+echo running gitleaks "$(gitleaks --version) with the following command👇"
+echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
+CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG)
 
 if [ $? -eq 1 ]
 then
