67efe2c01370b940ac0c0315232b8c118be0073a
Zachary <zricezrice@gmail.com>
2022-09-04 17:22:51 -0500

    just bump

--- dist/index.js
+++ dist/index.js
@@ -70564,7 +70564,6 @@ async function Scan(gitleaksEnableUploadArtifact, scanInfo, eventType) {
   core.info(`gitleaks cmd: gitleaks ${args.join(" ")}`);
   let exitCode = await exec.exec("gitleaks", args, {
     ignoreReturnCode: true,
-    outStream: process.stderr,
     delay: 60 * 1000,
   });
   core.setOutput("exit-code", exitCode);
